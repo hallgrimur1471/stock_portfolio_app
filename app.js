04ec1705bdec048f9a677057d5f80cb619e7ad0d
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-02-28 00:23:59 +0000

    Add initial roadmap items

--- app.js
+++ app.js
@@ -19,8 +19,25 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send('Hello, world!').end();
+
+  // TODO: redirect to /search/home
+});
+
+// Shows details of the <ticker> searched
+app.get("/search/<ticker>", (req, res) => {
+
+});
+
+// Displays the watchlist of the user
+app.get("/search/<ticker>", (req, res) => {
+
+});
+
+// Displays the portfolio of the user
+app.get("/portfolio", (req, res) => {
+
 });
 
 // Start the server
