5e5710dcab448d9ba379a08602ca901017a11be9
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-03-15 17:56:55 +0000

    Add API routes

--- app.js
+++ app.js
@@ -22,6 +22,138 @@ app.get("/api/example", (req, res) => {
   res.json({ example: "value" });
 });
 
+// Company's Description
+app.get("/api/description", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Historical Data
+app.get("/api/historical", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let timeInterval = 1;
+  let from = 163102248; // Unix timestamp
+  let to = 1631627048; // Unix timestamp
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=${timeInterval}&from=${from}&to=${to}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Latest Price of Stock
+app.get("/api/quote", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// For search autocomplete functionality
+app.get("/api/autocomplete", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/search?q=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's News
+app.get("/api/news", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let from = "2021-09-01"; // Date in YYYY-MM-DD
+  let to = "2021-09-09"; // Date in YYYY-MM-DD
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=${from}&to=${to}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Recommendation Trends
+app.get("/api/trends", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/recommendation?symbol=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Social Sentiment
+app.get("/api/social", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let from = "2022-01-01";
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/social-sentiment?symbol=${symbol}&from=${from}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Peers
+app.get("/api/peers", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/peers?symbol=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Company's Earnings
+// TODO: Replace null values for response keys with 0
+//       See specs 4.1.9.
+app.get("/api/earnings", (req, res, next) => {
+  let symbol = req.query.symbol;
+  let key = process.env.FINHUB_API_KEY;
+  let url = `https://finnhub.io/api/v1/stock/earnings?symbol=${symbol}&token=${key}`;
+
+  getFinnhub(url)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
+
+// Start the server
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
 async function getFinnhub(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (res) => {
@@ -49,40 +181,5 @@ async function getFinnhub(url) {
   });
 }
 
-// Company's Description
-app.get("/api/description", (req, res, next) => {
-  let symbol = req.query.symbol;
-  let key = process.env.FINHUB_API_KEY;
-  let url = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${key}`;
-
-  getFinnhub(url)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch(next);
-});
-
-// Shows details of the <ticker> searched
-app.get("/api/search/<ticker>", (req, res) => {
-
-});
-
-// Displays the watchlist of the user
-app.get("/api/search/<ticker>", (req, res) => {
-
-});
-
-// Displays the portfolio of the user
-app.get("/api/portfolio", (req, res) => {
-
-});
-
-// Start the server
-const PORT = parseInt(process.env.PORT) || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END gae_node_request_example]
-
 module.exports = app;
+// [END gae_node_request_example]
