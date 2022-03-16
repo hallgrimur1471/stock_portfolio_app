2e1b01b383d8a5bf87889ffef7664acd4bca1c34
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-03-16 19:42:56 +0000

    Install ng-bootstrap

--- stock_portfolio_frontend/src/app/app.module.ts
+++ stock_portfolio_frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { SearchComponent } from './search/search.component';
     MatNativeDateModule,
     MaterialExampleModule,
     ReactiveFormsModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
