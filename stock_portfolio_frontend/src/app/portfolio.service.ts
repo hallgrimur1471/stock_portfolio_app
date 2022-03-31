81f3b7389201cf23a69bde5bd1ff3a5d0bc8d8f7
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-03-31 18:40:25 +0000

    WIP on portfolio service

--- stock_portfolio_frontend/src/app/portfolio.service.ts
+++ stock_portfolio_frontend/src/app/portfolio.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Portfolio } from './portfolio';
 
@@ -13,7 +15,7 @@ export class PortfolioService {
       {
         ticker: 'VMW',
         name: 'VMware Inc',
-        quantity: 70.01,
+        quantity: 70.00,
         avgCost: 117.49,
         totalCost: 8224.30,
         change: 0.23,
@@ -36,6 +38,37 @@ export class PortfolioService {
   constructor() { }
 
   public getPortfolio(): Observable<Portfolio> {
+    return this.getPortfolioFromLocalStorage().pipe(
+      map(
+        p => this.updatePortfolio(p)
+      ),
+      tap(
+        p => this.portfolio = p
+      )
+    )
+  }
+
+  private getPortfolioFromLocalStorage(): Observable<Portfolio> {
+    // TODO: implement
     return of(this.portfolio);
+  }
+
+  private updatePortfolio(p: Portfolio): Portfolio {
+    p.shares.map(
+      (s) => {
+        s.currentPrice = 100000;
+      }
+    )
+    return p;
+  }
+
+  public buyShares(ticker: string, name: string, quantity: number, currentPrice: number): boolean {
+    // let s = getShare(ticker);
+    // let buyCost = quantity * s.currentPrice;
+    // if (this.money >= buyCost) {
+    //
+    // }
+
+    return true;
   }
 }
