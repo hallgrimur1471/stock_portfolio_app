44e66f864e165bf9c434dc653a938dec01de9b6b
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-03-31 13:11:42 +0000

    Add portfolio structure

---
+++ stock_portfolio_frontend/src/app/portfolio.ts
@@ -0,0 +1,6 @@
+import { Share } from './share';
+
+export interface Portfolio {
+    money: number
+    shares: Share[];
+}
---
+++ stock_portfolio_frontend/src/app/share.ts
@@ -0,0 +1,10 @@
+export interface Share {
+    ticker: string,
+    name: string,
+    quantity: number,
+    avgCost: number,
+    totalCost: number,
+    change: number,
+    currentPrice: number,
+    marketValue: number,
+}
