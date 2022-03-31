3cfd6d11e15fc192647aabc64bfb72faceaab190
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-03-31 17:55:28 +0000

    Implement mock portfolios

--- stock_portfolio_frontend/src/app/portfolio.service.ts
+++ stock_portfolio_frontend/src/app/portfolio.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Portfolio } from './portfolio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
+  portfolio: Portfolio = {
+    money: 8896.71,
+    shares: [
+      {
+        ticker: 'VMW',
+        name: 'VMware Inc',
+        quantity: 70.01,
+        avgCost: 117.49,
+        totalCost: 8224.30,
+        change: 0.23,
+        currentPrice: 117.72,
+        marketValue: 8240.40
+      },
+      {
+        ticker: 'GOOGL',
+        name: 'Alphabet Inc',
+        quantity: 3,
+        avgCost: 2626.33,
+        totalCost: 7878.99,
+        change: -2.64,
+        currentPrice: 2628.97,
+        marketValue: 7886.91,
+      }
+    ]
+  }
 
   constructor() { }
+
+  public getPortfolio(): Observable<Portfolio> {
+    return of(this.portfolio);
+  }
 }
--- stock_portfolio_frontend/src/app/portfolio/portfolio.component.ts
+++ stock_portfolio_frontend/src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Portfolio } from '../portfolio';
+import { PortfolioService } from '../portfolio.service';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  portfolio!: Portfolio;
 
-  constructor() { }
+  constructor(private ps: PortfolioService) { }
 
   ngOnInit(): void {
+    this.getPortfolio();
   }
 
+  private getPortfolio(): void {
+    this.ps.getPortfolio()
+      .subscribe(portfolio => this.portfolio = portfolio);
+  }
 }
